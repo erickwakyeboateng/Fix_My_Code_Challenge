940d76ac74b11c41042c0852a34762c3f913bb94
eric <boateng.eric44@gmail.com>
2023-08-24 17:06:20 +0000

    fixmycodechallenge

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,7 +6,8 @@
     of the program.
 */
 
-if (process.argv.length <= 2) {
+if (process.argv.length <= 2)
+{
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
@@ -15,13 +16,16 @@ if (process.argv.length <= 2) {
 
 const size = parseInt(process.argv[2]); // Removed the base (16) argument
 
-if (isNaN(size)) {
+if (isNaN(size))
+{
     process.stderr.write("Invalid size argument\n");
     process.exit(1);
 }
 
-for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
+for (let i = 0; i < size; i++)
+{
+    for (let j = 0; j < size; j++)
+	{
         process.stdout.write("#");
     }
     process.stdout.write("\n");
